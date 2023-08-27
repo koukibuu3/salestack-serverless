5a45f3d5301421b99f40745a7c9d6cdad87aaada
koukibuu3 <koukibuu3@gmail.com>
2023-08-27 18:12:31 +0900

    feat: 開始日時を扱う値オブジェクトを作成

---
+++ tests/domain/invoice/valueObjects/date/FromDateTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Domain\Invoice\ValueObjects\Date;
+
+use App\Domain\Invoice\ValueObjects\Date\FromDate;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group domain
+ * @group invoice
+ * @group valueObjects
+ * @group date
+ * @group fromDate
+ */
+final class FromDateTest extends TestCase
+{
+    private FromDate $fromDate;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->fromDate = new FromDate('2023/08/27');
+    }
+
+    /** @test */
+    public function 開始日時が取得できる(): void
+    {
+        $this->assertSame('2023-08-27', $this->fromDate->value);
+    }
+
+    /** @test */
+    public function 開始日時が空の場合は例外が発生する(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('fromDate is empty.');
+
+        new FromDate('');
+    }
+
+    /** @test */
+    public function 開始日時が不正な値の場合は例外が発生する(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('fromDate is invalid.');
+
+        new FromDate('2023/08/32');
+    }
+}
