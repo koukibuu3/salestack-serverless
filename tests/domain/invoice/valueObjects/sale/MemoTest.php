09bd6d999e2a460abde7930d1d4ae7de97b03241
koukibuu3 <koukibuu3@gmail.com>
2023-08-27 22:57:51 +0900

    feat: 備考を扱う値オブジェクトを作成

---
+++ tests/domain/invoice/valueObjects/sale/MemoTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Domain\Invoice\ValueObjects;
+
+use App\Domain\Invoice\ValueObjects\Sale\Memo;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group domain
+ * @group invoice
+ * @group valueObjects
+ * @group sale
+ * @group memo
+ */
+final class MemoTest extends TestCase
+{
+    private Memo $memo;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->memo = new Memo('メモ');
+    }
+
+    /** @test */
+    public function メモを取得できる(): void
+    {
+        $this->assertSame('メモ', $this->memo->value);
+    }
+
+    /** @test */
+    public function メモが50文字を超える場合は例外が発生する(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('memo length id expected 50, but 51.');
+
+        new Memo('123456789012345678901234567890123456789012345678901');
+    }
+}
